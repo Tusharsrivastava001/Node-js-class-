024bafc5babac8af2fd9e4cefdc0fff3317db653
Tushar srivastava <tusharsrivastava1725@gmail.com>
2025-10-13 14:56:51 +0530

    Update basic.js

--- basic.js
+++ basic.js
@@ -14,3 +14,25 @@ console.log(typeof(d));
 console.log(typeof(e));
 console.log(typeof(f));
 console.log("Try programiz.pro");
+
+function hello(){
+    console.log("Hello k23PB");
+}
+function arg(name){
+    console.log(name+" hi");
+}
+function add(a,b){
+    return a+b;
+}
+let greet=(a,b)=>{return a+b}; //this is the arror function yrr
+
+//anoloys function
+//callback me use hote 
+//event handlin me use hote hai
+setTimeout(function(){
+    console.log("Hello after 2 sec")
+},2000);
+hello();
+arg("htushat");
+console.log(add(2,88));
+console.log(greet(1,3));
