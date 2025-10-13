d098ee3816b35affe9ff12d3af61da87d78c371f
Tushar srivastava <tusharsrivastava1725@gmail.com>
2025-10-13 14:21:02 +0530

    Create basic.js

---
+++ basic.js
@@ -0,0 +1,16 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+let a=10;
+let b='a';
+let c="helloo";
+let d=a;
+let g="";
+let f=1234567890n;
+
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+console.log(typeof(f));
+console.log("Try programiz.pro");
