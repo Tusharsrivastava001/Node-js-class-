b30312767b9e751b06ad335d29d3626072f2e9ca
Tushar srivastava <tusharsrivastava1725@gmail.com>
2025-10-13 15:22:19 +0530

    Update basic.js

--- basic.js
+++ basic.js
@@ -1,20 +1,3 @@
-// Online Javascript Editor for free
-// Write, Edit and Run your Javascript code using JS Online Compiler
-let a=10;
-let b='a';
-let c="helloo";
-let d=a;
-let g="";
-let f=1234567890n;
-
-console.log(typeof(a));
-console.log(typeof(b));
-console.log(typeof(c));
-console.log(typeof(d));
-console.log(typeof(e));
-console.log(typeof(f));
-console.log("Try programiz.pro");
-
 function hello(){
     console.log("Hello k23PB");
 }
@@ -29,10 +12,21 @@ let greet=(a,b)=>{return a+b}; //this is the arror function yrr
 //anoloys function
 //callback me use hote 
 //event handlin me use hote hai
-setTimeout(function(){
-    console.log("Hello after 2 sec")
-},2000);
+
 hello();
 arg("htushat");
 console.log(add(2,88));
 console.log(greet(1,3));
+let arr=[1,2,3,46,44];
+for(let i in arr){
+    console.log(arr[i]);
+}
+console.log("Hello");
+for(let i of arr){
+    console.log(i);
+}
+arr1=[1,2,3,4,5,6,7];
+arr1.splice(3,2); //remove the two element from 2 index
+
+console.log(arr1);
+//
