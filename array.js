52c43b089d8d130fe1378747fa61c2ce42464441
Tushar srivastava <tusharsrivastava1725@gmail.com>
2025-10-14 15:52:39 +0530

    Create array.js

---
+++ array.js
@@ -0,0 +1,24 @@
+// //array in the js 
+// const a=[1,2,4];
+// const b=[12,3,4,3];
+// const m=a.concat(b);
+// console.log(a);
+// console.log(b);
+// console.log(m); // array 
+
+// const num1=[2,24,56];
+// console.log(num1.every(n=>n%2===0));
+
+
+// //filter();
+// const nums=[2,3,4];
+// const even=nums.filter(n=>n%2===0);
+// console.log(even);
+
+
+//for //each //eveery
+let arr=[1,2,3,4];
+//here is the push option
+arr.push(10);
+console.log(arr);
+
